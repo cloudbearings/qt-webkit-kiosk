080e992a084108ca6692ee0125046d01b2b7e0a1
sergey.dryabzhinsky@gmail.com <sergey.dryabzhinsky@gmail.com@45e420db-7a24-7704-dcb1-8fe70cd9efb0>
2012-04-17 19:23:16 +0000

    Tag version 1.02.00

---
+++ tags/qt-webkit-kiosk-1.02.00/src/webview.cpp
@@ -0,0 +1,45 @@
+#include "config.h"
+
+#include <QtGui>
+#include <QtWebKit>
+#include "webview.h"
+
+WebView::WebView(QWidget* parent)
+{
+    audioOutput = new Phonon::AudioOutput(Phonon::MusicCategory, this);
+    mediaObject = new Phonon::MediaObject(this);
+
+    mediaObject->setTickInterval(1000);
+
+    Phonon::createPath(mediaObject, audioOutput);
+
+    this->setParent(parent);
+}
+
+void WebView::setSettings(QSettings *settings)
+{
+    this->mainSettings = settings;
+}
+
+void WebView::mousePressEvent(QMouseEvent *event)
+{
+    QWebView::mousePressEvent(event);
+    if (event->button() == Qt::LeftButton) {
+        this->setFocus();
+        if (mainSettings->value("event-sounds/enable").toBool()) {
+            QString sound = mainSettings->value("event-sounds/window-clicked").toString();
+            qDebug() << "Clicked! Play sound: " << sound;
+            if (sound.length()) {
+
+                mediaObject->stop();
+                mediaObject->clearQueue();
+
+                Phonon::MediaSource source(sound);
+
+                mediaObject->setCurrentSource(source);
+                mediaObject->play();
+
+            }
+        }
+    }
+}
