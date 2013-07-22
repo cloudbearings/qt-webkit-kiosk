0b25cc006dc84f6dde1a8d1fbdfcc03b072db0be
sergey.dryabzhinsky@gmail.com <sergey.dryabzhinsky@gmail.com@45e420db-7a24-7704-dcb1-8fe70cd9efb0>
2013-07-22 18:59:46 +0000

    Tag 1.05.07

---
+++ tags/qt-webkit-kiosk-1.05.07/src/webview.h
@@ -0,0 +1,48 @@
+#ifndef WEBVIEW_H
+#define WEBVIEW_H
+
+#include <QtGui>
+#include <QtWebKit>
+#include <QtWebKitWidgets/QWebView>
+#include <QtWebKitWidgets/QWebFrame>
+#include <qplayer.h>
+#include <fakewebview.h>
+
+class WebView : public QWebView
+{
+    Q_OBJECT
+
+public:
+    explicit WebView(QWidget* parent = 0);
+
+    void setSettings(QSettings *settings);
+    void loadHomepage();
+    void initSignals();
+
+    void setPage(QWebPage* page);
+
+    QWebView *createWindow(QWebPage::WebWindowType type);
+
+    void playSound(QString soundSetting);
+
+public slots:
+    void handlePrintRequested(QWebFrame *);
+    void handleUrlChanged(const QUrl &);
+
+protected:
+    void mousePressEvent(QMouseEvent *event);
+    QPlayer *getPlayer();
+
+private:
+    QPlayer *player;
+    QSettings *mainSettings;
+    FakeWebView *loader;
+    QPrinter *printer;
+
+private slots:
+    void handleSslErrors(QNetworkReply* reply, const QList<QSslError> &errors);
+    void handleWindowCloseRequested();
+
+};
+
+#endif // WEBVIEW_H
